52a9286330f84ae802eb43ed391c3d462ddf3226
Dylan Nolasco <dylann@bluetrailsoft.com>
2024-02-09 12:25:43 -0600

    Build failure

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,16 @@
-import { z, defineCollection } from 'astro:content';
+// THERE IS A BUG IMPORTING THESE CONTENT SO I NEED TO FIGURE OUT HOW TI FIX IT
+// import { z, defineCollection } from 'astro:content';
 
-const blogCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    tags: z.array(z.string()),
-    date: z.date(),
-    description: z.string().optional(),
-  }),
-});
+// const blogCollection = defineCollection({
+//   type: 'content',
+//   schema: z.object({
+//     title: z.string(),
+//     tags: z.array(z.string()),
+//     date: z.date(),
+//     description: z.string().optional(),
+//   }),
+// });
 
-export const collections = {
-  blog: blogCollection,
-};
+// export const collections = {
+//   blog: blogCollection,
+// };
